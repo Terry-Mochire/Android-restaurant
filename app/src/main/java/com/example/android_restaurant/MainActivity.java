77289b6bec110e0e510de5f4c60dc43f30b37bad
Terry <terrybmochire@gmail.com>
2022-05-25 17:10:20 +0300

    Parse text between main and restaurant activities.

--- app/src/main/java/com/example/android_restaurant/MainActivity.java
+++ app/src/main/java/com/example/android_restaurant/MainActivity.java
@@ -6,22 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantsButton);
 
         mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class );
-
+                intent.putExtra("location", location);
                 startActivity(intent);
 
             }
