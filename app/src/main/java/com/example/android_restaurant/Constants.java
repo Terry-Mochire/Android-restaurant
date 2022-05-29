3644349abeb3e1ffc091c959c93a37ffee939c0b
Terry <terrybmochire@gmail.com>
2022-05-29 19:12:13 +0300

    Add API credentials.

---
+++ app/src/main/java/com/example/android_restaurant/Constants.java
@@ -0,0 +1,10 @@
+package com.example.android_restaurant;
+
+import android.os.Build;
+
+import androidx.core.os.BuildCompat;
+
+public class Constants {
+
+    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+}
