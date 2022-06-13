79c9404b25411ecf84b781797924fd2e9952c43e
Terry <terrybmochire@gmail.com>
2022-06-13 14:38:03 +0300

    Save restaurants to fiebase.

--- app/src/main/java/com/example/android_restaurant/ui/MainActivity.java
+++ app/src/main/java/com/example/android_restaurant/ui/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.android_restaurant.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,8 +15,11 @@ import android.widget.TextView;
 
 import com.example.android_restaurant.Constants;
 import com.example.android_restaurant.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mEditor;
     private DatabaseReference mSearchedLocationReference;
+    private ValueEventListener mSearchedLocationReferenceListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .getInstance()
                         .getReference()
                                 .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
+
+        mSearchedLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot locationSnapShot : snapshot.getChildren()) {
+                    String location = locationSnapShot.getValue().toString();
+                    Log.d("Locations Updated", "location: " + location);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // update UI here if error occurred.
+            }
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -43,6 +64,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 //        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 //             mEditor = mSharedPreferences.edit();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
     }
     @Override
     public void onClick(View v) {
--- app/src/main/java/com/example/android_restaurant/ui/RestaurantDetailFragment.java
+++ app/src/main/java/com/example/android_restaurant/ui/RestaurantDetailFragment.java
@@ -13,10 +13,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.android_restaurant.Constants;
 import com.example.android_restaurant.R;
 import com.example.android_restaurant.models.Business;
 import com.example.android_restaurant.models.Category;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
@@ -81,7 +85,7 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
         mPhoneLabel.setOnClickListener(this);
         mAddressLabel.setOnClickListener(this);
 
-
+        mSaveRestaurantButton.setOnClickListener(this);
         return view;
     }
 
@@ -104,6 +108,14 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
                             + "," + mRestaurant.getCoordinates().getLongitude()
                             + "?q=(" + mRestaurant.getName() + ")"));
             startActivity(mapIntent);
+        }
+
+        if (v == mSaveRestaurantButton) {
+            DatabaseReference restaurantRef = FirebaseDatabase
+                    .getInstance()
+                    .getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+            restaurantRef.push().setValue(mRestaurant);
+            Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT).show();
         }
 
     }
